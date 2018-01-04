d667cd1bc730f1597e48d696ab764ad4d236f151
Baran Usluel <me@baranusluel.com>
2018-01-03 23:55:40 -0500

    Added custom loading bar

--- Assets/WebGLTemplates/FullscreenTemplate/TemplateData/UnityProgress.js
+++ Assets/WebGLTemplates/FullscreenTemplate/TemplateData/UnityProgress.js
@@ -1,30 +1,3 @@
-function UnityProgress(gameInstance, progress) {
-  if (!gameInstance.Module)
-    return;
-  if (!gameInstance.logo) {
-    gameInstance.logo = document.createElement("p");
-	gameInstance.logo.innerHTML = "Loading...";
-    gameInstance.logo.className = "logo";
-    gameInstance.container.appendChild(gameInstance.logo);
-  }
-  /*
-  if (!gameInstance.progress) {    
-    gameInstance.progress = document.createElement("div");
-    gameInstance.progress.className = "progress " + gameInstance.Module.splashScreenStyle;
-    gameInstance.progress.empty = document.createElement("div");
-    gameInstance.progress.empty.className = "empty";
-    gameInstance.progress.appendChild(gameInstance.progress.empty);
-    gameInstance.progress.full = document.createElement("div");
-    gameInstance.progress.full.className = "full";
-    gameInstance.progress.appendChild(gameInstance.progress.full);
-    gameInstance.container.appendChild(gameInstance.progress);
-  }
-  gameInstance.progress.full.style.width = (100 * progress) + "%";
-  gameInstance.progress.empty.style.width = (100 * (1 - progress)) + "%";*/
-  if (progress == 1)
-    gameInstance.logo.style.display = "none";
-}
-
 function resized() {
   var canvas = document.getElementById("#canvas");
   var gameContainer = document.getElementById("gameContainer");
@@ -32,4 +5,51 @@ function resized() {
     canvas.width = gameContainer.clientWidth;
     canvas.height = gameContainer.clientHeight;
   }
+}
+
+
+
+// Following adapted from: https://ocias.com/blog/unity-webgl-custom-progress-bar/
+
+function init() {
+    
+}
+
+function UnityProgress (dom, progress) {
+  this.progress = 0.0;
+  this.message = "";
+  this.dom = dom;
+  
+  createjs.CSSPlugin.install(createjs.Tween);
+  createjs.Ticker.setFPS(60);
+  
+  var parent = dom.parentNode;
+  
+  this.SetProgress = function (progress) { 
+    if (this.progress < progress)
+      this.progress = progress; 
+    if (progress == 1) {
+      this.SetMessage("Preparing...");
+      document.getElementById("spinner").style.display = "inherit";
+      document.getElementById("bgBar").style.display = "none";
+      document.getElementById("progressBar").style.display = "none";
+    } 
+    this.Update();
+  }
+  
+  this.SetMessage = function (message) { 
+    this.message = message; 
+    this.Update();
+  }
+  this.Clear = function() {
+    document.getElementById("loadingBox").style.display = "none";
+  }
+  this.Update = function() {
+    var length = 200 * Math.min(this.progress, 1);
+    bar = document.getElementById("progressBar")
+    bar.style.width = length + "px";
+    document.getElementById("loadingInfo").innerHTML = this.message;
+  }
+  this.SetProgress(progress);
+  this.Update();
 }
