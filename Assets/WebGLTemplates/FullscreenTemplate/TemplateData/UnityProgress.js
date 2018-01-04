49f507511a3465b879ca2481af03e87e936fea21
Baran Usluel <me@baranusluel.com>
2018-01-04 00:46:10 -0500

    Fixed custom loading bar and template CSS

--- Assets/WebGLTemplates/FullscreenTemplate/TemplateData/UnityProgress.js
+++ Assets/WebGLTemplates/FullscreenTemplate/TemplateData/UnityProgress.js
@@ -16,40 +16,15 @@ function init() {
 }
 
 function UnityProgress (dom, progress) {
-  this.progress = 0.0;
-  this.message = "";
-  this.dom = dom;
-  
-  createjs.CSSPlugin.install(createjs.Tween);
-  createjs.Ticker.setFPS(60);
-  
-  var parent = dom.parentNode;
-  
   this.SetProgress = function (progress) { 
-    if (this.progress < progress)
-      this.progress = progress; 
-    if (progress == 1) {
-      this.SetMessage("Preparing...");
-      document.getElementById("spinner").style.display = "inherit";
-      document.getElementById("bgBar").style.display = "none";
-      document.getElementById("progressBar").style.display = "none";
-    } 
-    this.Update();
+    if (progress == 1)
+      document.getElementById("loadingBox").style.display = "none";
   }
-  
-  this.SetMessage = function (message) { 
-    this.message = message; 
-    this.Update();
-  }
-  this.Clear = function() {
-    document.getElementById("loadingBox").style.display = "none";
-  }
-  this.Update = function() {
-    var length = 200 * Math.min(this.progress, 1);
+  this.Update = function(progress) {
+    var length = 200 * Math.min(progress, 1);
     bar = document.getElementById("progressBar")
     bar.style.width = length + "px";
-    document.getElementById("loadingInfo").innerHTML = this.message;
   }
   this.SetProgress(progress);
-  this.Update();
+  this.Update(progress);
 }
